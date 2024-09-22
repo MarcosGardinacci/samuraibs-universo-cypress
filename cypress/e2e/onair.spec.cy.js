f6f3d4ca4cf83bb8e724b6ae275c4daba6ca00e3
Marcos Gardinacci <marcosgardinacci@yahoo.com.br>
2024-09-22 11:48:07 -0300

    Update onair.spec.cy.js
    Adicionando um simples comentário

--- cypress/e2e/onair.spec.cy.js
+++ cypress/e2e/onair.spec.cy.js
@@ -1,6 +1,7 @@
 describe('teste onair spec', () => {
   it('webapp deve estar online', () => {
-    
+    // um simples comentário
+        
     cy.visit('http://localhost:3000')
     cy.title()
        .should('eq', 'Samurai Barbershop by QAninja')
